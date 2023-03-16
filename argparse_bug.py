be0b3f721f69e3a32d5496db7ec4ede81103b186
William Murphy <william.murphy333@gmail.com>
2023-03-16 20:22:52 +0800

    Typo

--- argparse_bug.py
+++ argparse_bug.py
@@ -4,4 +4,4 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--foobar", type=str)
 args = parser.parse_args()
 
-print(f'temp_args.foobar: {args.foobar}')
+print(f'args.foobar: {args.foobar}')
